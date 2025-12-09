4e7f71b4f081bd1dbf244d6f7bbf168bf3749b88
Ahadul Haque Shovo <shovo2522@gmail.com>
2025-12-10 01:21:26 +0600

    Add advanced mathematical functions header file

---
+++ advanced.h
@@ -0,0 +1,26 @@
+#ifndef ADVANCED_H
+#define ADVANCED_H
+
+// Trigonometric functions
+double tangent(double x);
+double cotangent(double x);
+double secant(double x);
+double cosecant(double x);
+
+// Inverse trigonometric functions
+double arcsine(double x);
+double arccosine(double x);
+double arctangent(double x);
+
+// Advanced mathematical functions
+double square_root(double x);
+double cube_root(double x);
+double factorial(int n);
+double exponential(double x);
+double absolute_value(double x);
+
+// Logarithmic functions
+double log_base_10(double x);
+double log_base_n(double x, double base);
+
+#endif
