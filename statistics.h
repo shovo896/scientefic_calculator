f520c3f0dd553aec9137ba69d8819e35ddd62d23
Ahadul Haque Shovo <shovo2522@gmail.com>
2025-12-10 00:22:12 +0600

    Add statistics.h with statistical function declarations

---
+++ statistics.h
@@ -0,0 +1,14 @@
+
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
+// Statistical functions
+double calculate_mean(double arr[], int n);
+double calculate_median(double arr[], int n);
+double calculate_variance(double arr[], int n);
+double calculate_std_deviation(double arr[], int n);
+double find_max(double arr[], int n);
+double find_min(double arr[], int n);
+double calculate_sum(double arr[], int n);
+
+#endif
